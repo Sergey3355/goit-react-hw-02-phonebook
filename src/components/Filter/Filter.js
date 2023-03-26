9cfea5a3c4bac789677c45c47a1a0f643bc4d939
Sergey Chistiakov <chistykov2024@gmail.com>
2023-03-26 16:43:50 +0300

    дз2

---
+++ src/components/Filter/Filter.js
@@ -0,0 +1,16 @@
+import css from './Filter.module.css';
+const Filter = ({ value, onChange }) => {
+  return (
+    <label className={css.label}>
+      Find contacts by name
+      <input
+        className={css.input}
+        type="text"
+        value={value}
+        onChange={onChange}
+      />
+    </label>
+  );
+};
+
+export default Filter;
